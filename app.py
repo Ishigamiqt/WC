2f6cec6563efa973fe98877ba4478886a636ae7b
Ishigami <78327910+XCalever@users.noreply.github.com>
2024-05-19 02:58:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import numpy as np
 def load_model():
     model = tf.keras.models.load_model('model_weather.hdf5')
     return model
-
 def preprocess_image(image_data):
     img = Image.open(image_data)
     img = img.resize((244, 244))
@@ -16,7 +15,6 @@ def preprocess_image(image_data):
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
     return img
-
 def predict_weather(image_data, model):
     # Preprocess the image
     img = preprocess_image(image_data)
@@ -25,11 +23,10 @@ def predict_weather(image_data, model):
     return predicted_class
 
 weather_labels = {
-    0: 'Sunrise',
+    0: 'Cloudy',
     1: 'Rainy',
     2: 'Shine',
-    3: 'Cloudy'
-}
+    3: 'Sunrise'}
 
 def main():
     st.title('Weather Classifier System')
@@ -37,14 +34,11 @@ def main():
     file = st.file_uploader("Choose a weather photo from your computer", type=["jpg", "jpeg", "png"])
 
     if file is not None:
-        # Display the uploaded image
         image_display = Image.open(file)
         st.image(image_display, caption='Uploaded Image', use_column_width=True)
 
-        # Predict the weather condition
         predicted_class = predict_weather(file, model)
         predicted_label = weather_labels.get(predicted_class, 'Unknown')
-
         st.write(f"### Prediction: {predicted_label}")
 
 if __name__ == '__main__':
